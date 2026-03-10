baa1d83dce2f1f7daa3025387a0b57270a6b5dad
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-03-10 22:42:54 +1100

    Update TaskRepository.java

--- backend/src/main/java/com/todo/tasks/TaskRepository.java
+++ backend/src/main/java/com/todo/tasks/TaskRepository.java
@@ -1,27 +1,19 @@
 package com.todo.tasks;
-//import com.todo.Task; from the related entity
 
-//import org.springframework.scheduling.config.Task;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import com.todo.entity.Task;
-//JpaRepository provides standards for CRUD 
-public interface TaskRepository extends JpaRepository<Task, Integer>{
-    //<class that represents the data you want to persist in your database, type of the ID field in the entity class>
 
-    //The new keyword is only for instantiating an object in Java. We are deleting here, so no need to add the 'new' keyword.
-    //Distinct helps to remove duplicate
-     //Add queries (Flow: DB->Repo)
-    //Follow the Spring Data JPA naming convention
+public interface TaskRepository extends JpaRepository<Task, Integer>{
     List <Task> findByCategoryIdAndMakeArchivedFalse(Integer categoryId); 
 
-    //for simple queries (No group by, having, etc), better use methods than @query
     List <Task> findByMakeArchivedFalse();
 
     List <Task> findByMakeArchivedTrue();
    
     void deleteByCategoryId(Integer categoryId);
 }
+
