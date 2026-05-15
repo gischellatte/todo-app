d51fe98775da88e2b95a7c2b63fb4481e3d76e67
gischellatte <40295535+gischellatte@users.noreply.github.com>
2026-05-16 09:01:31 +1000

    Update TaskServiceTest.java

--- backend/src/test/java/com/todo/tasks/service/TaskServiceTest.java
+++ backend/src/test/java/com/todo/tasks/service/TaskServiceTest.java
@@ -30,25 +30,25 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class) 
-//class name must ends with a 'Test', otherwise Maven wont pick it up
+
 class TaskServiceTest {
 
-    @Mock //@Mock - creates fake objects from dependencies. Mockito makes a “fake” version ofTaskRepository (false dependency)
+    @Mock
     private TaskRepository taskRepository;
     @Mock
     private CategoryRepository categoryRepository;
 
 
-    @InjectMocks //@InjectMocks - create the object you want to test , then everything @Mockwill be automatically inserted
+    @InjectMocks
     private TaskService taskService;
 
 
     @Test
     public void testingGetAllTasks_categId() {
-        when(taskRepository.findByMakeArchivedFalse()).thenReturn(List.of(new Task()));//almost everything in the unit test uses when(...).thenReturn(...)
+        when(taskRepository.findByMakeArchivedFalse()).thenReturn(List.of(new Task()));
         List<Task> tasks = taskService.getAllTasks(null);
         assertNotNull(tasks);
-        verify(taskRepository, times(1)).findByMakeArchivedFalse();//verifies  taskRepo calls findByMakeArchivedFalse() once
+        verify(taskRepository, times(1)).findByMakeArchivedFalse();
     }
 
     @Test
@@ -81,8 +81,7 @@ class TaskServiceTest {
     
 
     when(categoryRepository.findById(1)).thenReturn(Optional.of(mockCategory));
-    when(taskRepository.save(any(Task.class))).thenReturn(mockTask); //save() also returns data, not just actions
-    //most modern repos expects a returned object (if your service method is not a void method) - chatGPT
+    when(taskRepository.save(any(Task.class))).thenReturn(mockTask); 
     taskService.createTask(postTasksDto1);
 
     verify(taskRepository, times(1)).save(any(Task.class)); 
